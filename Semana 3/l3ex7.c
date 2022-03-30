8d58ccbbcbfa839bcd75bb5784e5034a666fec67
Lui Gill Aquini <lgaquini@gmail.com>
2022-03-29 22:50:25 -0300

    Estrutura

--- Semana 3/l3ex7.c
+++ Semana 3/l3ex7.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define CHAR (sizeof(char))
+#define INT (sizeof(int))
 
 typedef struct
 {
@@ -9,8 +11,35 @@ typedef struct
   int altura;
 } Pessoa;
 
+void *incluir(void *pBuffer);
+void *apagar(void *pBuffer);
+void buscar(void *pBuffer);
+void listar(void *pBuffer);
+
 int main()
 {
 
+  Pessoa pessoas[10];
+  void *pBuffer;
+
   return 0;
+}
+
+void *incluir(void *pBuffer)
+{
+  printf("Digite o nome da pessoa:\n");
+  printf("Digite a idade da pessoa:\n");
+  printf("Digite o telefone da pessoa:\n");
+}
+
+void *apagar(void *pBuffer)
+{
+}
+
+void buscar(void *pBuffer)
+{
+}
+
+void listar(void *pBuffer)
+{
 }
